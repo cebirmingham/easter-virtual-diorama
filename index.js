5a3157e0602ad64d2d810d5714c730eae10ef25d
Charlotte Birmingham <charlotte.birmingham@ft.com>
2020-04-10 17:28:59 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require ('express');
+const expressHandlebars = require('express-handlebars');
+const question = require('./lib/getQuestionToBeAsked');
+const app = express();
+
+app.engine('hbs', expressHandlebars({
+  defaultLayout: 'main',
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', (req, res) => {
+  res.render('beach', {
+    title: 'beach'
+  })
+});
+
+app.get('/question', (req, res) => {
+  if (req.query.id) {
+    const selectedQuestion = question(req.query.id);
+    res.json(selectedQuestion ? selectedQuestion : {tired: true});
+  }
+  else {
+    res.status(400);
+    res.json({error: 'no id'})
+  }
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Listening on http://localhost:' + (process.env.PORT || 3000));
+});
---
+++ public/client.js
@@ -0,0 +1,23 @@
+const characters = document.querySelectorAll('[data-character-id]');
+
+async function askQuestion({target}) {
+  const characterId = target.dataset.characterId;
+  const fetchResponse = await fetch(`/question?id=${characterId}`);
+  const response = await fetchResponse.json();
+
+  if (response.tired) {
+    console.log('man i am super tired');
+  }
+  else if (response.finished) {
+    console.log('looks like i\'m the only one still standing - maybe we should stop! i am here in a professional capacity after all.');
+  }
+  else if (response.question) {
+    console.log(response.question);
+  }
+
+
+
+}
+
+
+[ ...characters].forEach(el => el.addEventListener('click', askQuestion));
